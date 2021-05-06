3fa45ccd46d26788e476e63f709570a8e1da9769
DannyX424 <dee_mo16@yahoo.com>
2021-05-06 13:12:48 -0700

    Add project files.

---
+++ TrueCulture1/Models/PersonModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TrueCulture1.Models
+{
+    public class PersonModel
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        //add a birthday
+        public string EmailAddress { get; set; }
+    }
+}
